aff74ad67bfc381ab0948afd31ad7013274d1e90
j_h <13113017500>
2019-06-05 16:44:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paycamerademo/ScreenUtils.java
@@ -0,0 +1,39 @@
+package com.example.paycamerademo;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+class ScreenUtils {
+
+    /**
+     * sp转px.
+     *
+     * @param value   the value
+     * @param context the context
+     * @return the int
+     */
+    public static int sp2px(Context context, float value) {
+        Resources r;
+        if (context == null) {
+            r = Resources.getSystem();
+        } else {
+            r = context.getResources();
+        }
+        float spvalue = value * r.getDisplayMetrics().scaledDensity;
+        return (int) (spvalue + 0.5f);
+    }
+
+    /**
+     * dp转 px.
+     *
+     * @param value   the value
+     * @param context the context
+     * @return the int
+     */
+    public static int dp2px(Context context, float value) {
+        final float scale = context.getResources().getDisplayMetrics().densityDpi;
+        return (int) (value * (scale / 160) + 0.5f);
+    }
+
+
+}
